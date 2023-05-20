9c7d14f20ca428ae1b06dd9621efb25075df71bf
Margub Murshed <murshedmargub@gmail.com>
2023-05-21 00:36:47 +0600

    Login page added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,11 @@
 import {createBrowserRouter} from 'react-router-dom'
 import PrimaryLayout from '../layouts/PrimaryLayout'
 import Home from '../pages/Home/Home'
+import Login from '../pages/Login/Login'
 
 export const router = createBrowserRouter([
     {path: '/', element: <PrimaryLayout />, children: [
-        {index: true, element: <Home />}
+        {index: true, element: <Home />},
+        {path:'login', element: <Login />},
     ]}
 ])
