59deab26f1bdfa928bcf318a0362616c84effd09
Margub Murshed <murshedmargub@gmail.com>
2023-05-21 00:24:30 +0600

    Auth Provider added and set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import {RouterProvider} from 'react-router-dom'
-import './index.css'
-import { router } from './routes/Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./routes/Router";
+import AuthProvider from "./providers/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
